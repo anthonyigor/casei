8ac019541b53163810290abb14ddf4bbd32f199d
Anthony Igor <igor.torrestj11@gmail.com>
2024-01-09 18:02:32 -0300

    add update User

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,98 @@
+import { Request, Response } from "express";
+import { userRepository } from "../repositories/userRepository";
+import bcrypt from 'bcrypt';
+import { User } from "../entities/User";
+
+export class UserController {
+
+    async create(req: Request, res: Response) {
+        const { nome, email, password, nome_parceiro, data_casamento } = req.body
+
+        if (!nome || nome === undefined) {
+            return res.status(400).json({ message: 'Nome é obrigatório!'})
+        }
+
+        if (!email || email === undefined) {
+            return res.status(400).json({ message: 'Email é obrigatório!'})
+        }
+        
+        const emailExists = await userRepository.findOne({where: {email}})
+        if (emailExists) {
+            return res.status(400).json({ message: 'Email já cadastrado!'})
+        }
+
+        if (!password || password === undefined) {1
+            return res.status(400).json({ message: 'Password é obrigatório!'})
+        }
+        const hashPassword = await bcrypt.hash(password, 10)
+
+        try {   
+            const user = userRepository.create({
+                nome,
+                email,
+                password: hashPassword,
+                nome_parceiro,
+                data_casamento
+            })
+            await userRepository.save(user)
+            return res.status(201).json({message: "Usuário criado com sucesso!"})
+        } catch (error) {
+            return res.status(500).json({message: "Internal Server Error"})
+        }
+    }
+
+    async update(req: Request, res: Response) {
+        const { nome, email, password, nome_parceiro, data_casamento } = req.body;
+        const id = req.params.id;
+        
+        try {
+            let user = await userRepository.findOne({ where: { id: Number(id) } });
+            if (!user) {
+                return res.status(404).json({ message: "Usuário não encontrado!" });
+            }
+        
+            const updateFields = {} as User;
+
+            if (nome !== undefined && nome !== user.nome) {
+                updateFields.nome = nome;
+            }
+        
+            if (email !== undefined && email !== user.email) {
+                const emailExists = await userRepository.findOne({ where: { email } });
+                if (emailExists) {
+                    return res.status(400).json({ message: 'Email já cadastrado!' });
+                }
+                updateFields.email = email;
+            }
+        
+            if (password !== undefined) {
+                const hashPassword = await bcrypt.hash(password, 10);
+                updateFields.password = hashPassword;
+            }
+        
+            if (nome_parceiro !== undefined && nome_parceiro !== user.nome_parceiro) {
+                updateFields.nome_parceiro = nome_parceiro;
+            }
+        
+            if (data_casamento !== undefined && data_casamento !== user.data_casamento) {
+                updateFields.data_casamento = data_casamento;
+            }
+            
+            if (Object.keys(updateFields).length === 0) {
+                return res.status(400).json({ message: "Nenhum campo foi alterado." });
+            }
+            
+            await userRepository.update({ id: Number(id) }, updateFields);
+            
+            user = await userRepository.findOne({ where: { id: Number(id) } }); 
+            
+            return res.status(200).json({ message: "Usuário atualizado com sucesso!", user });
+        } catch (error) {
+            console.error(error);
+            return res.status(500).json({ message: "Ocorreu um erro ao atualizar o usuário." });
+        }
+        
+
+    }
+
+}
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { UserController } from "../controlllers/UserController";
+import { UserController } from "../controllers/UserController";
 
 const router = Router()
 const userController = new UserController()
 
 router.post('/create', userController.create)
+router.patch('/update/:id', userController.update)
 
 export default router
