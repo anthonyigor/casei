2c9b7a98d6cfad0c874a36b218682ef94e1b0dea
Anthony Igor <igor.torrestj11@gmail.com>
2024-10-05 11:31:36 -0300

    add: Loading em presentes

--- frontend/src/app/presentes/index/page.tsx
+++ frontend/src/app/presentes/index/page.tsx
@@ -8,6 +8,7 @@ import { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 import axios from "axios";
 import toast from "react-hot-toast";
+import LoadingModal from "@/app/components/LoadingModal";
 
 const greatVibes = Great_Vibes({ weight:'400', subsets: ['latin'] });  
 
@@ -15,9 +16,15 @@ const url = process.env.NEXT_PUBLIC_BACKEND_URL
 
 const Presentes = () => {
     const [presentes, setPresentes] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
 
     const router = useRouter()
     const session = useSession()
+
+    const onAddPresente = () => {
+        router.push('/presentes/adicionar')
+        setIsLoading(true)
+    }
 
     useEffect(() => {
         if (session.data?.user) {
@@ -30,20 +37,23 @@ const Presentes = () => {
             })
             .then(response => {
                 setPresentes(response.data)
+                setIsLoading(false)
             })
             .catch(error => {
                 toast.error('Erro ao buscar presentes')
+                setIsLoading(false)
             })
         }
     }, [session.data?.user])
     
     return (
        <>
+            {isLoading && <LoadingModal />}     
             <div className={greatVibes.className}>
                 <h2 className="text-5xl text-center mt-4 text-teal-600 font-semibold py-2">Lista de Presentes</h2>
             </div>
             <div className="fixed top-0 right-0 m-4">
-                <Button type="button" onClick={() => router.push('/presentes/adicionar')}>
+                <Button type="button" onClick={onAddPresente}>
                     Adicionar
                 </Button>
             </div>
