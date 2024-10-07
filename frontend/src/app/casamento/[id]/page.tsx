0b746ec318a6f8adc1e2eb2a919c37816419937a
anthonyigor <igor.torrestj11@gmail.com>
2024-10-07 15:57:55 -0300

    add: backgrou em menu convidado

--- frontend/src/app/casamento/[id]/page.tsx
+++ frontend/src/app/casamento/[id]/page.tsx
@@ -14,27 +14,28 @@ const Casamento = ({ params }: { params: IParams }) => {
     const userId = params.id;
 
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-white">
+        <div className="relative flex flex-col items-center justify-center min-h-screen bg-center bg-no-repeat bg-cover" style={{ backgroundImage: "url('/img/background.jpg')" }}>
+        {/* Camada de sobreposição com transparência */}
+        <div className="absolute inset-0 bg-black bg-opacity-10 z-0"></div>
             
             {/* Título */}
-            <div className={`${greatVibes.className} text-center mt-8`}>
-                <h1 className= 'text-5xl text-teal-600 font-semibold'>
+            <div className={`${greatVibes.className} text-center mt-14`}>
+                <h1 className= 'text-5xl text-teal-800 font-semibold'>
                     José e Maria
                 </h1>
             </div>
 
             {/* Envolope */}
-            <div className="mt-8">
-                <button className="relative w-40 h-28 flex items-center justify-center bg-white border-2 border-gray-200 rounded-lg shadow-lg">
-                    <Image src='/img/envelope.png' alt="Envelope" width={200} height={160}/>
+            <div className="mt-12">
+                <button className="relative w-40 h-28 flex items-center justify-center bg-transparent rounded-lg shadow-lg">
+                    <Image src='/img/envelope.png' alt="Envelope" width={160} height={160}/>
                 </button>
                 <span className="text-gray-500 text-sm mx-6">Toque para abrir</span>
             </div>
 
             {/* Data */}
             <div className="text-center mt-6 text-gray-600">
-                <p className="text-lg">SETEMBRO</p>
-                <p className="text-2xl font-bold">15</p>
+                <p className="text-2xl font-bold">15 SETEMBRO</p>
                 <p className="text-lg">DOM | 16h00 | 2024</p>
             </div>
 
