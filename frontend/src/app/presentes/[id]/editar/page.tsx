a5db444bc0c7816ce79db8a9604851d0d751e67e
anthonyigor <igor.torrestj11@gmail.com>
2025-03-11 17:36:59 -0300

    att: responsividade dos botoes

--- frontend/src/app/presentes/[id]/editar/page.tsx
+++ frontend/src/app/presentes/[id]/editar/page.tsx
@@ -47,7 +47,7 @@ const EditarPresente = ({ params }: { params: IParams }) => {
          <div className={greatVibes.className}>
             <h2 className="text-5xl text-center mt-4 text-teal-600 font-semibold py-2">Editar presente</h2>
         </div>
-        <div className="fixed top-0 lg:left-48 m-4">
+        <div className="hidden lg:block fixed top-0 lg:left-48 m-4">
                 <Button type="button" onClick={() => router.push('/presentes/index')}>
                     Voltar
                 </Button>
