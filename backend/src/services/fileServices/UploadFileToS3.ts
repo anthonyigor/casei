2fd2a970affb961ed30bfd0ee590168683853a21
anthonyigor <igor.torrestj11@gmail.com>
2025-03-20 17:27:49 -0300

    att: await s3.saveFile

--- backend/src/services/fileServices/UploadFileToS3.ts
+++ backend/src/services/fileServices/UploadFileToS3.ts
@@ -3,7 +3,7 @@ import { S3Storage } from "../../utils/S3Storage";
 export class UploadFileToS3 {
     async execute(filename: string, contentType: string): Promise<string> {
         const s3 = new S3Storage()
-        const fileUrl = s3.saveFile(filename, contentType)
+        const fileUrl = await s3.saveFile(filename, contentType)
         return fileUrl
     }
 }
